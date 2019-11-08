9af7fd10e527361f907831c1d82164f047927e48
jcoppens <jcoppens@ghgsat.com>
2019-11-08 16:48:45 -0500

    Initial commit for minesweeper solver

---
+++ src/AI.h
@@ -0,0 +1,23 @@
+// SRC: J.Coppens 2019
+
+#ifndef SWEEPERAI
+#define SWEEPERAI
+
+class MinesweeperAI {
+
+    public:
+        void read_board(void){
+
+        }
+
+        void click_tile(void){
+
+        }
+
+    private:
+        void compute(void){
+
+        }
+};
+
+#endif
