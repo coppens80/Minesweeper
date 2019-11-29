f061006a5d0f508f63f13823dfe5150636bbd4fa
jcoppens <jcoppens@ghgsat.com>
2019-11-29 11:10:42 -0500

    Allows user to set the game mode from the command line when running the program (ex. '$./test Medium') If no game mode is specifed, the mode is set to 'Easy' by default. If an unavailable mode is entered the game exits

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,22 @@
 #include <sstream>
 #include "minesweeper.h"
 #include "AI.h"
-
-int a = 0, b = 0;
+#include <stdlib.h>
 
 int main(int argc, char** argv){
-    
-    Minesweeper game("Easy");
+
+    std::string difficulty;
+    if (argc > 1)
+        if (std::string(argv[1]) == "Easy" || std::string(argv[1]) == "Medium" || std::string(argv[1]) == "Hard")
+            difficulty = argv[1];
+        else{
+            std::cout << "Error: Invalid game mode. Available modes: Easy, Medium, Hard" << std::endl;
+            exit(1);
+        }
+    else
+        difficulty = "Easy";
+
+    Minesweeper game(difficulty);
     MinesweeperAI solver(&game);
     auto [width, height] = game.window_size;
     
@@ -30,7 +40,7 @@ int main(int argc, char** argv){
                 window.close();
             
             if (event.type == sf::Event::MouseButtonPressed && !game.game_over){
-                std::cout << event.mouseButton.y << " " << event.mouseButton.x << std::endl;
+                /* std::cout << event.mouseButton.y << " " << event.mouseButton.x << std::endl; */
                 if (event.mouseButton.button == sf::Mouse::Left)
                     game.left_click(event.mouseButton.x, event.mouseButton.y);
                 if (event.mouseButton.button == sf::Mouse::Right)
@@ -43,10 +53,9 @@ int main(int argc, char** argv){
                     solver.compute();
                 }
                 if (event.key.code == sf::Keyboard::R){
-                    a = b = 0;
                     printf("reset\n");
                     game.reset();
-                    std::cout << "Average frame time: " << timer.getElapsedTime().asSeconds() * 1000 / n << std::endl;
+                    /* std::cout << "Average frame time: " << timer.getElapsedTime().asSeconds() * 1000 / n << std::endl; */
                     timer.restart();
                 }
             }
